8f4c1b06f92772dc6e44c4873ac17cfa0be21bc4
Colin Lloyd <clloyd12@gmail.com>
2015-09-06 14:35:02 -0400

    updating routes for api namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,9 @@
 Rails.application.routes.draw do
+  # Api definition
+  # maps 'api' to the directory 'api' under the controllers directory
+  namespace :api, defaults: { format: :json },
+            constraints: { subdomain: 'api' }, path: '/'
+  do
+    # We are going to list our resources here
+  end
 end
